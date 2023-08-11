52fd0aa41dd42401ff5508bce7fb962087298c02
Narmin Valiyeva <valiyevana@gmail.com>
2023-08-11 17:03:45 +0400

    h

--- HelloAutocode.java
+++ HelloAutocode.java
@@ -1,5 +1,5 @@
 public class HelloAutocode {
-  public static void main(String []) args {
-     System.out.print("Hello, Autocode");
+  public static void main(String[] args) {
+     System.out.print("Hello, Autocode!");
   }
 }
